7320dfcb1e0302db4aa4225e5a10aa61b3b68f27
blagisquet <baptiste.lagisquet@imie.fr>
2020-04-14 12:30:17 +0200

    added rules for dark mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { ThemeProvider } from 'styled-components';
 
 import Layout from '../components/layout';
+import ThemeButton from '../theme/themeButton';
+import { lightTheme, darkTheme } from '../theme/themes';
 
-const IndexPage = () => <Layout />;
+const IndexPage = () => {
+  const [theme, setTheme] = useState(darkTheme);
+
+  const handleTheme = () => {
+    theme.name === 'light' ? setTheme(darkTheme) : setTheme(lightTheme)
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ThemeButton handleTheme={handleTheme} />
+      <Layout />
+    </ThemeProvider>
+  )
+};
 
 export default IndexPage;
