2609b6ef79e63884030954c793cbbdedf28fdfb6
blagisquet <baptiste.lagisquet@imie.fr>
2020-05-19 10:08:22 +0200

    hover effect on map and artists is working

--- src/components/findUs.js
+++ src/components/findUs.js
@@ -12,12 +12,11 @@ const FindUs = ({ className }) => {
   return (
     <div className={className}>
       <h2 className="is-size-2 has-text-centered has-text-weight-bold">Nous trouver</h2>
-      <a href="https://goo.gl/maps/NLDzBV8N3Qf8wB9E8" target="_blank" rel="noopener noreferrer">
-        <div className="image imageMob has-text-centered is-hidden-tablet">
-          <h4 className="title is-size-3">Voir sur Google Maps</h4>
-        </div>
-        <div className="image imageDesk has-text-centered is-hidden-mobile">
-          <h4 className="title is-size-3">Voir sur Google Maps</h4>
+      <a className="tile" href="https://goo.gl/maps/NLDzBV8N3Qf8wB9E8" target="_blank" rel="noopener noreferrer">
+        <div className="imageDesk is-hidden-mobile"></div>
+        <div className="imageMob is-hidden-tablet"></div>
+        <div className="info">
+          <div className="textHover is-size-3">Voir sur Google Maps</div>
         </div>
       </a>
       <span id="contact"></span>
@@ -30,38 +29,32 @@ export default styled(FindUs)`
     margin-top: ${pxToRem(48)};
     margin-bottom: ${pxToRem(24)}
   }
+
+  .tile:hover .imageDesk {
+    opacity: 0.3;
+  }
+
+  .tile:hover .imageMob {
+    opacity: 0.3;
+  }
+
   .imageMob {
     background: url(${mapMob}) center/cover no-repeat;
     height: 20rem;
-    &:hover {
-      background: url(${mapMob}) center/cover no-repeat rgba(232, 232, 232, 0.5);
-      opacity: 0.4;
-      transition: 0.3s;
-    }
+    opacity: 1;
+    display: block;
+    width: 100%;
+    transition: .5s ease;
+    backface-visibility: hidden;
   }
+
   .imageDesk {
     background: url(${map}) center/cover no-repeat;
     height: 20rem;
-    &:hover {
-      background: url(${map}) center/cover no-repeat rgba(232, 232, 232, 0.5);
-      opacity: 0.4;
-      transition: 0.3s;
-    }
+    opacity: 1;
+    display: block;
+    width: 100%;
+    transition: .5s ease;
+    backface-visibility: hidden;
   }
-  h4 {
-    align-items: center;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    opacity: 0;
-    transition: 0.3s;
-  }
-  a {
-    position: relative;
-
-  }
-  a:hover h4 {
-      opacity: 1;
-    }
 `;
